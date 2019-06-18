ae58022bbb1c2cf64662b47c3610048aa157fb20
KingDDDD <simeonrau@gmx.de>
2019-06-18 15:42:45 +0200

    CarTestAdded
    Car tested and Sound added. Also mailbox light off after Collision

---
+++ Assets/Scripts/CarStraight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarStraight : MonoBehaviour
+{
+
+    public GameObject go;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        go.transform.position = go.transform.position + new Vector3(0, 0, -0.25f);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EndFence"))
+            Destroy(go);
+    }
+}
--- Assets/Scripts/MailboxFlag.cs
+++ Assets/Scripts/MailboxFlag.cs
@@ -6,6 +6,7 @@ public class MailboxFlag : MonoBehaviour
 {
 
     public GameObject go;
+    public GameObject halo;
 
     private bool up;
     // Start is called before the first frame update
@@ -26,6 +27,7 @@ public class MailboxFlag : MonoBehaviour
         {
             go.transform.Rotate(new Vector3(0, 1, 0), -90);
             go.transform.localPosition = new Vector3(go.transform.localPosition.x - 0.25f, go.transform.localPosition.y , go.transform.localPosition.z + 0.25f);
+            halo.SetActive(false);
             up = true;
         }
     }
