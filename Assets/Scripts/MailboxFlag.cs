2ef4a864472feaba16ceaa6f86e33cf99b2fd457
cschierle <c.schierle@gmx.de>
2019-06-28 12:58:05 +0200

    replaced mailbox game objects with prefab

--- Assets/Scripts/MailboxFlag.cs
+++ Assets/Scripts/MailboxFlag.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class MailboxFlag : MonoBehaviour
 {
 
-    public GameObject go; //Flag
+    public GameObject flag;
     public GameObject halo;
+    public PlayerController player;
 
     private bool up;
     // Start is called before the first frame update
@@ -36,8 +37,8 @@ public class MailboxFlag : MonoBehaviour
         {
             // GameController.setScore(score += 1)
             // Destroy(other.gameObject);
-            go.transform.Rotate(new Vector3(0, 1, 0), -90);
-            go.transform.localPosition = new Vector3(go.transform.localPosition.x - 0.25f, go.transform.localPosition.y, go.transform.localPosition.z + 0.25f);
+            flag.transform.Rotate(new Vector3(0, 1, 0), -90);
+            flag.transform.localPosition = new Vector3(flag.transform.localPosition.x - 0.25f, flag.transform.localPosition.y, flag.transform.localPosition.z + 0.25f);
             halo.SetActive(false);
             up = true;
         }
