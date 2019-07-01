2bf02db34a83ecdb4c798fbc2ccae759db483c3d
KingDDDD <simeonrau@gmx.de>
2019-07-01 19:58:20 +0200

    Farben der Mailbox

--- Assets/Scripts/BonusPoints.cs
+++ Assets/Scripts/BonusPoints.cs
@@ -18,7 +18,7 @@ public class BonusPoints : MonoBehaviour
             flag.transform.Rotate(new Vector3(0, 1, 0), -90);
             flag.transform.localPosition = new Vector3(flag.transform.localPosition.x - 0.25f, flag.transform.localPosition.y, flag.transform.localPosition.z + 0.25f);
             // light up in red to visualize bonus score
-            halo.GetComponent<Light>().color = Color.red;
+            halo.GetComponent<Light>().color = Color.green;
             mailboxFlag.up = true;
         }       
     }
